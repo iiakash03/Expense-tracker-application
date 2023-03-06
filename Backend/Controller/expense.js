3320b3f33598419696515c9c4ab133036e7cb4fa
iiakash03 <iiakash59@gmail.com>
2023-03-06 14:42:51 +0530

    updated all the errors

--- Backend/Controller/expense.js
+++ Backend/Controller/expense.js
@@ -101,9 +101,10 @@ const addExpense=async (req,res,next)=>{
 
 
 const getExpenses=async (req,res,next)=>{
+    try{
     let page=req.query.page || 1;
 
-    const ITEMS_PER_PAGE=req.query.rows || 3;
+    const ITEMS_PER_PAGE=req.query.rows || 5;
 
     const count=await expense.count({
         where:{
@@ -119,10 +120,13 @@ const getExpenses=async (req,res,next)=>{
     //console.log('bwhjdv3e3ydgriyg4r',count);
     let totalItems;
 
-
     const expenses=await expense.findAll({
         offset:(page-1)*ITEMS_PER_PAGE,
-        limit:ITEMS_PER_PAGE<count ?ITEMS_PER_PAGE:count,
+        limit: +ITEMS_PER_PAGE,
+
+        where:{
+            userId:req.user.id
+        }
     })
     console.log(expenses);
 
@@ -135,9 +139,11 @@ const getExpenses=async (req,res,next)=>{
         previousPage:page-1,
         lastPage:Math.ceil(totalItems/ITEMS_PER_PAGE),
     })
+}catch(err){
+    console.log(err);
+}
     
 }
-
 const deleteExpense=async (req,res,next)=>{
     const t=await sequelize.transaction();
 
